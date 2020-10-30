f26e5341d3d8f7f6439873fe203e7285ba2af3ec
Кирилл Кириленко <k.kirilenko@lucky.online>
2020-10-31 00:47:46 +0300

    Initial Commit

---
+++ BusinessAssistant/BusinessAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessAssistantApp.swift
+//  BusinessAssistant
+//
+//  Created by godemodegame on 31.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct BusinessAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
